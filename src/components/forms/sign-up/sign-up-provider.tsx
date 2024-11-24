7219b39fced2f00f159b9189b6fc0ad58f3c9494
Prajwal-PL <prajwalpl096@gmail.com>
2024-11-24 21:36:42 +0530

    Adding authentication with OTP, Adding image files and color configurations

---
+++ src/components/forms/sign-up/sign-up-provider.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const SignUpProvider = ({ children }: Props) => {
+  return <div>SignUpProvider</div>;
+};
+
+export default SignUpProvider;
